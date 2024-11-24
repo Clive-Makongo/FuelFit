3f7535ac2312f0fed5dd1ab7c5686e6f1ab52b89
Clive-Makongo <clivemakongo@gmail.com>
2024-11-24 20:38:35 +0000

    switching between mobile and desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Card from './components/manual/card'
-import Navbar from './components/manual/NavDesk/Navbar'
+import Home from './containers/Home'
 import './index.css'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="bg-blue-500 p-4 text-white">Test Styling</div>
-      <Card/>
+      <Home/>
     </>
   )
 }
---
+++ src/containers/Home.tsx
@@ -0,0 +1,34 @@
+import Card from '../components/manual/card';
+import NavDesk from '../components/manual/NavDesk/Navbar';
+import { useState } from 'react';
+import { useWindowSize } from '@/utils/useWindowSize';
+
+function Home(): JSX.Element {
+    const { width } = useWindowSize();
+
+    // Define a breakpoint (e.g., 768px for mobile vs. desktop)
+    const isMobile: boolean = width < 768;
+
+    return (
+        <>
+            <>
+                {console.log(width, isMobile)}
+            </>
+            {isMobile ? (
+                <div>
+                    {/* Mobile View */}
+                    <div className="bg-red-500 p-4 w-full text-white">Mobile View</div>
+                </div>
+            ) : (
+                <div>
+                    {/* Desktop View */}
+                    <NavDesk />
+                    <div className="bg-blue-500 p-4 text-white">Desktop View</div>
+                    <Card />
+                </div>
+            )}
+        </>
+    );
+}
+
+export default Home;
