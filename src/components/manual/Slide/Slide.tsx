e6c03c0ca84ed470cbf1a719af4374ba1174febf
Clive-Makongo <clivemakongo@gmail.com>
2024-12-07 00:35:02 +0000

    updating slide elements with const from react render

--- src/components/manual/Slide/Slide.tsx
+++ src/components/manual/Slide/Slide.tsx
@@ -1,34 +1,53 @@
 // Need to get breakfast lunch and dinner
 import { useState } from "react";
 import MealSegment from "../Meal/MealSegment";
+import { motion, AnimatePresence } from 'motion/react';
+import pic1 from '../../../assets/bg-image.jpg'
+import pic2 from '../../../assets/light.avif'
+import pic3 from '../../../assets/construction.jpeg'
 
-interface SlideProps {
-    imgSrc: string, // will also be API response
-    calories: number // will be API response
-};
+// map images to pics (for now)
 
 const meals: string[] = ['breakfast', 'lunch', 'dinner'];
 
+const picMap: Record<string, string> = {
+    'breakfast': pic1,
+    'lunch': pic2,
+    'dinner': pic3
+};
+ 
 
 const Slide = (): JSX.Element => {
-    const [currentSlide, setCurrentSlide] = useState<SlideProps[]>([]); // array holding breakfast lunch and dinner
     const [slideIndex, setSlideIndex] = useState<number>(0); // track which slide we are on
     // Need a function to swap the slides
     // held in state
 
     const handleForward = () => {
-        setSlideIndex(prev => prev === meals.length - 1 ? 0 : prev + 1)
+        setSlideIndex(prev => prev === meals.length - 1 ? 0 : prev + 1);
     };
 
     const handleBackward = () => {
-        setSlideIndex(prev => prev === 0 ? meals.length - 1 : prev - 1)
+        setSlideIndex(prev => prev === 0 ? meals.length - 1 : prev - 1);
     };
 
+    const currentMeal = meals[slideIndex];
+    const currentImage = picMap[currentMeal];
 
     const renderCurrentSlide = (): JSX.Element => {
 
         return (
-            <MealSegment meal={meals[slideIndex]} />
+            <AnimatePresence>
+                <motion.div
+                    exit={{ scale: 2 }}
+                    whileHover={{ scale: 1.015 }}
+                    className="flex flex-col border-black border  text-center meal-segment rounded-lg m-2 h-full"
+                >
+                    <img className='w-full' src={currentImage} alt="meal-image" />
+                    <h1 className='p-4  font-bold w-1/2'>{meals[slideIndex]}</h1>
+                    <p className="text-2xl p-4 text-center font-bold bg-red-500">Data</p>
+                </motion.div>
+            </AnimatePresence>
+
         );
     };
     // use motion to swap the images
@@ -42,9 +61,9 @@ const Slide = (): JSX.Element => {
 
     return (
         <>
-            {renderCurrentSlide()} 
+            {renderCurrentSlide()}
             <button onClick={handleBackward}>Prev Slide</button>
-            <button onClick={handleForward}> Next Slide</button>  
+            <button onClick={handleForward}> Next Slide</button>
         </>
     )
 };
