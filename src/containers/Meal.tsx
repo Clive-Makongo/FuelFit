19730c9e093e0640ddb57c84fd86fb97794406de
Clive-Makongo <clivemakongo@gmail.com>
2025-02-02 01:06:53 +0000

    added API call

--- src/containers/Meal.tsx
+++ src/containers/Meal.tsx
@@ -1,15 +1,43 @@
+import { useEffect, useState } from "react";
 import GenerateMeal from "@/components/manual/Meal/GenerateMeal";
 import MealSegment from "@/components/manual/Meal/MealSegment";
 import Slide from "@/components/manual/Slide/Slide";
 import { useWindowSize } from "@/utils/useWindowSize";
+import calories from "../utils/coloriesApi";
+import { flushSync } from "react-dom";
 
 const meals: string[] = ['breakfast', 'lunch', 'dinner'];
 
+interface Data {
+    calories: number;
+    diet: string
+};
 
 const Meal = (): JSX.Element => {
+    const [caloryData, setCaloryData] = useState<Data>({ calories: 0, diet: "" })
+    const [load, setLoad] = useState<boolean>(false)
     const { width } = useWindowSize();
     const isMobile: boolean = width < 768;
     // API CALLS WITH IMAGES AND CALORIE DATA
+
+    const handleGenerateMeal = async () => {
+        console.log("Hello World")
+        try {
+            setLoad(true);
+            const response = await calories(2000, 'keto');
+            console.log(response)
+            setCaloryData(response)
+            console.log(caloryData, 'Calory Data')
+            
+        } catch (error) { 
+            console.log("Error fetching meal data")
+        }
+        finally {
+            setLoad(false)
+        }
+    }
+
+
     return (
         <>
             {/* Mobile Version */}
@@ -22,24 +50,38 @@ const Meal = (): JSX.Element => {
                     >
                         <GenerateMeal info="number" placeholder="Enter calories" />
                         <GenerateMeal info="text" placeholder="Enter meal details" />
+                        
                     </section>
                     <div className="flex flex-col md:flex-row justify-evenly w-full md:w-4/5">
                         {/* Render the slide for the mobile view */}
+                        <button
+                            onClick={handleGenerateMeal}
+                            className="px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-600 transition-colors"
+                        >
+                            NAME
+                        </button>
                         <Slide />
                     </div>
                 </div>
             ) : (
-                    // DESKTOP VERSION
+                // DESKTOP VERSION
                 <div className="bg-yellow-500">
                     <section
                         className="inputs flex flex-row justify-evenly w-4/5 mb-6"
                     >
                         <GenerateMeal info="number" placeholder="Enter calories" />
                         <GenerateMeal info="text" placeholder="Enter meal details" />
+                            <button
+                                onClick={handleGenerateMeal}
+                                className="px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-600 transition-colors"
+                            >
+                                Get Data
+                            </button>
                     </section>
-                    <div className="grid grid-cols-3 gap-2 w-full">
+                        <div className="grid grid-cols-3 gap-2 w-full">
+                           
                         {meals.map((meal, id) => (
-                            <MealSegment key={id} meal={meal}/>
+                            <MealSegment key={id} meal={meal} />
                         ))}
                     </div>
 
